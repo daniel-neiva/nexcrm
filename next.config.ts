33f5a0f0201ea333393474d72cb7b64b6a664069
Daniel Neiva <daniel@Daniels-MacBook-Air.local>
2026-02-21 17:56:30 -0300

    Fix syntax for bypassing ESLint in Next.js 16.1 on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // @ts-expect-error: NextConfig type definition in 16.1 doesn't include eslint/typescript properties by default but it's supported by the underlying Webpack plugin.
+  // @ts-expect-error: Bypassing strict typescript validation for ESLint config in Next.js 16.1.
   eslint: {
     ignoreDuringBuilds: true,
   },
